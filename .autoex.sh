2ef6059b60aee2b6cd48cfdd588141e780a204e9
zhuangqiubin <zhuangqiubin@allwinnertech.com>
2018-03-09 09:49:12 +0800

    tar can auto detect the format, just need tar xvf

--- .autoex.sh
+++ .autoex.sh
@@ -6,13 +6,13 @@ ex () {
                print -P ”       Extract the file specified based on the extension”
         elif [[ -f $1 ]] ; then
            case $1 in
-             *.tar)       tar xvf  $1     ;;
-             *.tbz2)      tar xvjf $1    ;;
-             *.tgz)       tar xvzf $1    ;;
-             *.tar.bz2)   tar xjvf $1    ;;
-             *.tar.gz)    tar xvzf $1    ;;
-             *.tar.xz)    tar Jxf  $1    ;;
-             *.tar.Z)     tar xvZf $1    ;;
+             *.tar)       tar xvf  $1    ;;
+             *.tbz2)      tar xvf  $1    ;;
+             *.tgz)       tar xvf  $1    ;;
+             *.tar.bz2)   tar xvf  $1    ;;
+             *.tar.gz)    tar xvf  $1    ;;
+             *.tar.xz)    tar xvf  $1    ;;
+             *.tar.Z)     tar xvf  $1    ;;
              *.bz2)       bunzip2v $1    ;;
              *.rar)       rar x $1       ;;
              *.gz)        gunzip $1      ;;
@@ -27,3 +27,12 @@ ex () {
          echo “‘$1’ is not a valid file”
        fi
     }
+
+
+             # *.tar)       tar xvf  $1     ;;
+             # *.tbz2)      tar xvjf $1    ;;
+             # *.tgz)       tar xvzf $1    ;;
+             # *.tar.bz2)   tar xjvf $1    ;;
+             # *.tar.gz)    tar xvzf $1    ;;
+             # *.tar.xz)    tar Jxf  $1    ;;
+             # *.tar.Z)     tar xvZf $1    ;;
